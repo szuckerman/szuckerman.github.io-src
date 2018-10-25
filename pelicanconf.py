5cf61edac40a53a2df5ea3ae63e071f6aff8b166
Sam Zuckerman <sam.p.zuckerman@gmail.com>
2018-10-25 16:47:21 -0400

    Add pyjanitor contribution

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,18 +38,21 @@ THEME = "pelican-elegant-1.3"
 DISQUS_SITENAME = 'szuckerman-github-io-1'
 
 
-LANDING_PAGE_ABOUT = {'title': 'I am full stack developer focusing on data-driven applications', 'details': 'I work as a full stack developer analyst using various database and web technologies to make data analysis more efficient. I work with the whole pipeline, from data engineering and database design to front end UX.'}
+LANDING_PAGE_ABOUT = {'title': 'I am full stack developer focusing on data-driven applications', 'details': 'I work as '
+                      'a full stack developer analyst using various database and web technologies to make data analysis '
+                      'more efficient. I work with the whole pipeline, from data engineering and database design to '
+                      'front end UX.'}
 
 PROJECTS = [{
     'name': 'Bookscouter Linear Optimizer',
     'url': 'https://github.com/szuckerman/Bookscouter_LP',
     'description':
-    'A program which scrapes bookscouter.com for given ISBN numbers and determines to which stores they should be sent to maximize profit.'},
-    {'name': 'Elegant Theme for Pelican',
-    'url': 'http://oncrashreboot.com/pelican-elegant',
-    'description': 'A clean and distraction free theme, with search and a'
-    ' lot more unique features, using Jinja2 and Bootstrap'}]
-
+    'A program which scrapes bookscouter.com for given ISBN numbers and determines to which stores they should be sent'
+    'to maximize profit.'},
+    {'name': 'PyJanitor',
+    'url': 'https://github.com/ericmjl/pyjanitor',
+    'description': '(Contributor) A Python implementation of the R package <a href="https://github.com/sfirke/janitor" target="_blank"'
+                   '>janitor</a>, and more. It includes tools for examining and cleaning dirty data.'}]
 
 
 # Uncomment following line if you want document-relative URLs when developing
